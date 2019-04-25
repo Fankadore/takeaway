a9ef8ee84b87cbdd8dcf3a9c95002a812ea6749a
Fankadore <rmackenzie88@gmail.com>
2019-04-25 12:29:37 +0100

    Refactor

--- client/src/data/content.js
+++ client/src/data/content.js
@@ -1,98 +1,35 @@
 const content = {
 	name: "Takeaway Ltd",
 	copyright: "© 2019 Takeaway Ltd",
-	contact: {
-		address: {
-			street: "123 Fake Street",
-			city: "Faketon",
-			postcode: "FT12 3FT",
-			latlng: [57.284566, -2.374106]
-		},
-		phone: "01234 567890",
-		email: "food@takeawayltd.com",
-	},
-	menu: [
+	locations: [
 		{
-			title: "Starters",
-			description: "These starters are all very tasty and come with a crusty roll.",
-			items: [
-				{
-					name: "Starter 1 with heaps of shit and extra stuff and a side of chips",
-					description: "Starter 1 is lovely. blah blah blah blah blah blah blah blah blah blah blah blah blah.",
-					price: 4.95
-				},
-				{
-					name: "Starter 2",
-					description: "Starter 2 is lovely as well.",
-					price: 3.95
-				}
-			]
+			address: {
+				name: "Takeaway Ltd",
+				street: "123 Fake Street",
+				city: "Faketon",
+				postcode: "FT12 3FT",
+			},
+			latlng: [57.284566, -2.374106],
+			phone: "01234 567890",
+			email: "food@takeawayltd.com",
 		},
 		{
-			title: "Mains",
-			description: "Blah Blah",
-			items: [
-				{
-					name: "Main 1",
-					description: "Main 1 is lovely.",
-					price: 8.50
-				},
-				{
-					name: "Main 2",
-					description: "Main 2 is lovely as well.",
-					price: 12.95
-				}
-			]
+			address: {
+				name: "Takeaway Ltd",
+				street: "420 Real Street",
+				city: "Acity",
+				postcode: "AB12 3CD",
+			},
+			latlng: [60.284566, -3.374106],
+			phone: "01234 098765",
+			email: "food2@takeawayltd.com",
 		},
-		{
-			title: "Sides",
-			description: "Blah Blah",
-			items: [
-				{
-					name: "Chips",
-					description: "Lengths of tattie that have been fried.",
-					price: 1.95
-				},
-				{
-					name: "Onion Rings",
-					description: "Battered onions that have been fried.",
-					price: 1.95
-				}
-			]
-		},
-		{
-			title: "Desserts",
-			description: "Blah Blah",
-			items: [
-				{
-					name: "Dessert 1",
-					description: "Dessert 1 is lovely.",
-					price: 4.95
-				},
-				{
-					name: "Dessert 2",
-					description: "Dessert 2 is lovely as well.",
-					price: 3.95
-				}
-			]
-		},
-		{
-			title: "Drinks",
-			description: "",
-			items: [
-				{
-					name: "Coca-Cola",
-					description: "",
-					price: 1.00
-				},
-				{
-					name: "Pepsi",
-					description: "",
-					price: 1.00
-				}
-			]
-		}
-	]
+	],
+	nav: [
+		{name: "Home", href: "/"},
+		{name: "Order", href: "/order"},
+		{name: "Find Us", href: "/find-us"},
+	],
 };
 
 export default content;
