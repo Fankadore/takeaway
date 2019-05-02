3a52797be9e52a99ab3a89910bbf2e123cd38438
Fankadore <rmackenzie88@gmail.com>
2019-05-02 17:13:10 +0100

    Improved formating

--- client/src/components/PriceInput/PriceInput.js
+++ client/src/components/PriceInput/PriceInput.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './PriceInput.scss';
 
 const PriceInput = (props) => {
 	const { id, update, value, currency, edit } = props;
 	const price = parseFloat(value).toFixed(2);
-	const currencyId = id + "-currency";
-	const valueId = id + "-value";
 
-	const parsePrice = (e) => e.target.value = parseFloat(e.target.value).toFixed(2);
+	const resize = (e) => {
+		update(e);
+		const digits = e.target.value.toString().length - 1; // count digits discounting decimal point
+		e.target.style.width = ((digits * 8) + 2) + 'px'; // convert to pixels
+	};
+	
+	const parsePrice = (e) => {
+		e.target.value = parseFloat(e.target.value).toFixed(2);
+		resize(e);
+	}
+	
+	useEffect(() => {
+		if (edit) {
+			const inputFields = document.getElementsByClassName(id + "-value");
+			Array.from(inputFields).forEach(inputField => {
+				inputField.value = parseFloat(inputField.value).toFixed(2);
+			});
+		}
+	}, [edit]);
 
 	return (
-		<div id={id} className="price-input">
-			<p id={currencyId} className="price-input__currency">{currency}</p>
+		<div id={id} className={id + " price-input"}>
+			<p id={id + "-currency"} className={id + "-currency price-input__currency"}>{currency}</p>
 			{(edit)
-				? <input id={valueId} type="number" step="0.01" className="price-input__input" onFocus={parsePrice} onBlur={parsePrice} onChange={update} value={value} placeholder={0} />
-				: <p id={valueId} className="price-input__display">{price}</p>
+				? <input id={id + "-value"} type="number" step="0.01" className={id + "-value price-input__input"} onFocus={parsePrice} onBlur={parsePrice} onChange={resize} value={value} placeholder="0.00" />
+				: <p id={id + "-value"} className={id + "-value price-input__display"}>{price}</p>
 			}
 		</div>
 	);
