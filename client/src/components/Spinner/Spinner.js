9dce772e77db28be3e0706469f8c786dce3ca72c
Fankadore <fankadore@protonmail.com>
2019-08-15 19:48:04 +0100

    Refactor

--- client/src/components/Spinner/Spinner.js
+++ client/src/components/Spinner/Spinner.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Spinner.scss';
 
-function Spinner(props) {
-	const { id } = props;
-	const textId = id + "-text";
-
+const Spinner = ({ id }) => {
 	return (
 		<div id={id} className={id + " spinner"}>
-			<p id={textId} className={id + "-text spinner-text"}>Loading</p>
+			<p id={id + "-text"} className={id + "-text spinner-text"}>Loading</p>
 		</div>
 	);
-}
+};
 
 export default Spinner;
